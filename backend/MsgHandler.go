90cab3a22974670a547316d44ef54bf9c1f6e4f2
sh1luo <690898835@qq.com>
2020-03-29 01:37:42 +0800

    V0.0.3

--- backend/MsgHandler.go
+++ backend/MsgHandler.go
@@ -5,10 +5,10 @@ import (
 	"github.com/go-qamel/qamel"
 )
 
-//func init() {
-//	RegisterQmlConnectFeedBack("ConnectFeedBack", 1, 0, "ConnectFeedBack")
-//	RegisterQmlHandleMsg("HandleMsg", 1, 0, "HandleMsg")
-//}
+func init() {
+	RegisterQmlConnectFeedBack("ConnectFeedBack", 1, 0, "ConnectFeedBack")
+	RegisterQmlHandleMsg("HandleMsg", 1, 0, "HandleMsg")
+}
 
 type ConnectFeedBack struct {
 	qamel.QmlObject
@@ -51,60 +51,60 @@ type HandleMsg struct {
 }
 
 // 处理各种需要发送到 QML 的消息
-//func (h *HandleMsg) init() {
-//	go func() {
-//		for {
-//			select {
-//			// 处理用户弹幕
-//			case a := <-bilibili.P.DanMu:
-//				if e, err := GetDanMu(a); err != nil {
-//					s, err := json.Marshal(e)
-//					if err != nil {
-//						continue
-//					}
-//					h.sendDanMu(string(s))
-//				}
-//			// 处理用户礼物
-//			case b := <-bilibili.P.Gift:
-//				if e := GetGift(b); e != nil {
-//					s, err := json.Marshal(e)
-//					if err != nil {
-//						continue
-//					}
-//					h.sendGift(string(s))
-//				}
-//			// 处理贵宾进场，如老爷
-//			case c := <-bilibili.P.WelCome:
-//				w := GetWelCome(c, 1)
-//				s, err := json.Marshal(w)
-//				if err != nil {
-//					continue
-//				}
-//				h.sendWelCome(string(s))
-//			// 处理房管进场
-//			case d := <-bilibili.P.WelComeGuard:
-//				w := GetWelCome(d, 2)
-//				s, err := json.Marshal(w)
-//				if err != nil {
-//					continue
-//				}
-//				h.sendWelComeGuard(string(s))
-//			// 处理舰长等贵宾进场
-//			case e := <-bilibili.P.GreatSailing:
-//				w := GetWelCome(e, 3)
-//				s, err := json.Marshal(w)
-//				if err != nil {
-//					continue
-//				}
-//				h.sendGreatSailing(string(s))
-//			// 处理关注数变动消息
-//			case f := <-bilibili.P.Fans:
-//				i := json.Get(f, "data", "fans").ToInt()
-//				h.sendFansChanged(i)
-//			// 处理在线人气变动处理
-//			case g := <-bilibili.P.Online:
-//				h.sendOnlineChanged(g)
-//			}
-//		}
-//	}()
-//}
+func (h *HandleMsg) init() {
+	go func() {
+		for {
+			select {
+			// 处理用户弹幕
+			case a := <-bilibili.P.DanMu:
+				if e := GetDanMu(a); e == nil {
+					s, err := json.Marshal(e)
+					if err != nil {
+						continue
+					}
+					h.sendDanMu(string(s))
+				}
+			// 处理用户礼物
+			case b := <-bilibili.P.Gift:
+				if e := GetGift(b); e != nil {
+					s, err := json.Marshal(e)
+					if err != nil {
+						continue
+					}
+					h.sendGift(string(s))
+				}
+			// 处理贵宾进场，如老爷
+			case c := <-bilibili.P.WelCome:
+				w := GetWelCome(c, 1)
+				s, err := json.Marshal(w)
+				if err != nil {
+					continue
+				}
+				h.sendWelCome(string(s))
+			// 处理房管进场
+			case d := <-bilibili.P.WelComeGuard:
+				w := GetWelCome(d, 2)
+				s, err := json.Marshal(w)
+				if err != nil {
+					continue
+				}
+				h.sendWelComeGuard(string(s))
+			// 处理舰长等贵宾进场
+			case e := <-bilibili.P.GreatSailing:
+				w := GetWelCome(e, 3)
+				s, err := json.Marshal(w)
+				if err != nil {
+					continue
+				}
+				h.sendGreatSailing(string(s))
+			// 处理关注数变动消息
+			case f := <-bilibili.P.Fans:
+				i := json.Get(f, "data", "fans").ToInt()
+				h.sendFansChanged(i)
+			// 处理在线人气变动处理
+			case g := <-bilibili.P.Online:
+				h.sendOnlineChanged(g)
+			}
+		}
+	}()
+}
